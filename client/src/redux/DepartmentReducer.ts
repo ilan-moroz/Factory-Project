ce4a4aa47f21b87ae870596623e51464ddf56792
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-27 13:44:12 +0300

    departmentReducer

--- client/src/redux/DepartmentReducer.ts
+++ client/src/redux/DepartmentReducer.ts
@@ -1,1 +1,79 @@
-export {};
+import { Department } from "../models/Department";
+
+export interface DepartmentState {
+  departments: Department[];
+}
+
+export enum DepartmentActionType {
+  getAllDepartments = "getAllDepartments",
+  addDepartment = "addDepartment",
+  updateDepartment = "updateDepartment",
+  deleteDepartment = "deleteDepartment",
+}
+
+export interface DepartmentAction {
+  type: DepartmentActionType;
+  payload?: any;
+}
+
+export const getAllDepartmentsAction = (
+  departments: Department[]
+): DepartmentAction => {
+  return { type: DepartmentActionType.getAllDepartments, payload: departments };
+};
+
+export const addDepartmentAction = (
+  department: Department
+): DepartmentAction => {
+  return { type: DepartmentActionType.addDepartment, payload: department };
+};
+
+export const updateDepartmentAction = (
+  department: Department
+): DepartmentAction => {
+  return { type: DepartmentActionType.updateDepartment, payload: department };
+};
+
+export const deleteDepartmentAction = (id: string): DepartmentAction => {
+  return { type: DepartmentActionType.deleteDepartment, payload: id };
+};
+
+export const departmentReducer = (
+  currentState: DepartmentState = { departments: [] },
+  action: DepartmentAction
+): DepartmentState => {
+  const state = { ...currentState };
+
+  switch (action.type) {
+    case DepartmentActionType.getAllDepartments:
+      state.departments = action.payload;
+      break;
+
+    case DepartmentActionType.addDepartment:
+      state.departments.push(action.payload);
+      break;
+
+    case DepartmentActionType.updateDepartment:
+      const updatedDepartmentIndex = state.departments.findIndex(
+        (department) => department.id === action.payload.id
+      );
+      if (updatedDepartmentIndex !== -1) {
+        const updatedDepartment = {
+          ...state.departments[updatedDepartmentIndex],
+          name: action.payload.name,
+        };
+        state.departments[updatedDepartmentIndex] = updatedDepartment;
+      }
+      break;
+
+    case DepartmentActionType.deleteDepartment:
+      state.departments = state.departments.filter(
+        (department) => department.id !== action.payload.id
+      );
+      break;
+
+    default:
+      return currentState;
+  }
+  return state;
+};
--- client/src/redux/Store.ts
+++ client/src/redux/Store.ts
@@ -2,16 +2,27 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { userReducer } from "./UserReducer";
+import { departmentReducer } from "./DepartmentReducer";
 
 const userPersistConfig = {
   key: "users",
   storage,
 };
 
+const departmentPersistConfig = {
+  key: "departments",
+  storage,
+};
+
 const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+const persistedDepartmentReducer = persistReducer(
+  departmentPersistConfig,
+  departmentReducer
+);
 
 const reducers = {
   users: persistedUserReducer,
+  departments: persistedDepartmentReducer,
 };
 
 const store = configureStore({
