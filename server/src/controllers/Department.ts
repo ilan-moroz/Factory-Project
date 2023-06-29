ea7f8d7b18e22b8455b38f677ce8ce9eb5afeb50
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-29 14:18:06 +0300

    comments

--- server/src/controllers/Department.ts
+++ server/src/controllers/Department.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { Department, Employee } from "../models/Factory";
 
-// GET /departments
+// GET /departments/getAllDepartments
 export const getAllDepartments = async (req: Request, res: Response) => {
   try {
     const departments = await Department.find();
@@ -11,56 +11,48 @@ export const getAllDepartments = async (req: Request, res: Response) => {
   }
 };
 
-// POST /departments
+// POST /departments/addDepartment
 export const createDepartment = async (req: Request, res: Response) => {
   try {
     const { name } = req.body;
-
     // Check if department with the same name already exists
     const existingDepartment = await Department.findOne({ name });
     if (existingDepartment) {
       return res.status(400).json({ error: "Department already exists" });
     }
-
     const newDepartment = new Department({ name });
     newDepartment.save();
-
     res.status(201).json(newDepartment);
   } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
 };
 
-// PUT /departments/:id
+// PUT /departments/updateDepartment/:id
 export const updateDepartment = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { name } = req.body;
-
     const department = await Department.findById(id);
     if (!department) {
       return res.status(404).json({ error: "Department not found" });
     }
-
     department.name = name;
     const updatedDepartment = await department.save();
-
     res.status(200).json(updatedDepartment);
   } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
 };
 
-// DELETE /departments/:id
+// DELETE /departments/deleteDepartment/:id
 export const deleteDepartment = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-
     const department = await Department.findById(id);
     if (!department) {
       return res.status(404).json({ error: "Department not found" });
     }
-
     // Check if the department has any employees
     const employeesInDepartment = await Employee.countDocuments({
       departmentId: id,
@@ -70,9 +62,7 @@ export const deleteDepartment = async (req: Request, res: Response) => {
         .status(400)
         .json({ error: "Cannot delete department with employees" });
     }
-
     await department.deleteOne();
-
     res.status(200).json({ message: "Department deleted successfully" });
   } catch (err: any) {
     res.status(500).json({ error: err.message });
