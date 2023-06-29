42f431cd9dac8c25417371c43dc0cbd3073cfb98
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-29 15:17:41 +0300

    employeeReducer

--- client/src/redux/EmployeeReducer.ts
+++ client/src/redux/EmployeeReducer.ts
@@ -1,1 +1,68 @@
-export interface Something {}
+import { Employee } from "../models/Employee";
+
+export interface EmployeeState {
+  employees: Employee[];
+}
+
+export enum EmployeeActionType {
+  getAllEmployees = "getAllEmployees",
+  addEmployee = "addEmployee",
+  updateEmployee = "updateEmployee",
+  deleteEmployee = "deleteEmployee",
+}
+
+export interface EmployeeAction {
+  type: EmployeeActionType;
+  payload?: any;
+}
+
+export const getAllEmployeesAction = (
+  employees: Employee[]
+): EmployeeAction => {
+  return { type: EmployeeActionType.getAllEmployees, payload: employees };
+};
+
+export const addEmployeeAction = (employee: Employee): EmployeeAction => {
+  return { type: EmployeeActionType.addEmployee, payload: employee };
+};
+
+export const updateEmployeeAction = (employee: Employee): EmployeeAction => {
+  return { type: EmployeeActionType.updateEmployee, payload: employee };
+};
+
+export const deleteEmployeeAction = (id: string): EmployeeAction => {
+  return { type: EmployeeActionType.deleteEmployee, payload: id };
+};
+
+export const employeeReducer = (
+  currentState: EmployeeState = { employees: [] },
+  action: EmployeeAction
+): EmployeeState => {
+  const state = { ...currentState };
+
+  switch (action.type) {
+    case EmployeeActionType.getAllEmployees:
+      state.employees = action.payload;
+      break;
+
+    case EmployeeActionType.addEmployee:
+      state.employees = [...state.employees, action.payload];
+      break;
+
+    case EmployeeActionType.updateEmployee:
+      state.employees = state.employees.map((employee) =>
+        employee._id === action.payload._id ? action.payload : employee
+      );
+      break;
+
+    case EmployeeActionType.deleteEmployee:
+      state.employees = state.employees.filter(
+        (employee) => employee._id !== action.payload
+      );
+      break;
+
+    default:
+      return currentState;
+  }
+  return state;
+};
--- client/src/redux/Store.ts
+++ client/src/redux/Store.ts
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage";
 import { userReducer } from "./UserReducer";
 import { departmentReducer } from "./DepartmentReducer";
 import { shiftReducer } from "./ShiftReducer";
+import { employeeReducer } from "./EmployeeReducer";
 
 const userPersistConfig = {
   key: "users",
@@ -20,17 +21,27 @@ const shiftPersistConfig = {
   storage,
 };
 
+const employeePersistConfig = {
+  key: "employees",
+  storage,
+};
+
 const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
 const persistedDepartmentReducer = persistReducer(
   departmentPersistConfig,
   departmentReducer
 );
 const persistedShiftReducer = persistReducer(shiftPersistConfig, shiftReducer);
+const persistedEmployeeReducer = persistReducer(
+  employeePersistConfig,
+  employeeReducer
+);
 
 const reducers = {
   users: persistedUserReducer,
   departments: persistedDepartmentReducer,
   shifts: persistedShiftReducer,
+  employees: persistedEmployeeReducer,
 };
 
 const store = configureStore({
