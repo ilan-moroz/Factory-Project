6aa49a6b205d4b967e512469d1415b4dd371c438
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-11 12:27:06 +0300

    delete unused actions

--- client/src/redux/UserReducer.ts
+++ client/src/redux/UserReducer.ts
@@ -2,18 +2,11 @@ import { User } from "../models/User";
 
 export interface UserState {
   user: User | null;
-  allUsers: User[];
   token: string | null;
-  isAdmin?: boolean;
 }
 
 export enum UserActionType {
-  updateUser = "updateUser",
-  getAllUsers = "getAllUsers",
-  deleteUser = "deleteUser",
-  addUser = "addUser",
   setLogout = "setLogout",
-  setIsAdmin = "setIsAdmin",
   setLogin = "setLogin",
 }
 
@@ -22,28 +15,8 @@ export interface UserAction {
   payload?: any;
 }
 
-export const updateUserAction = (user: User): UserAction => {
-  return { type: UserActionType.updateUser, payload: user };
-};
-
-export const getAllUsersAction = (users: User[]): UserAction => {
-  return { type: UserActionType.getAllUsers, payload: users };
-};
-
-export const deleteUserAction = (id: string): UserAction => {
-  return { type: UserActionType.deleteUser, payload: id };
-};
-
-export const addUserAction = (user: User): UserAction => {
-  return { type: UserActionType.addUser, payload: user };
-};
-
 export const setLogoutAction = (): UserAction => {
   return { type: UserActionType.setLogout };
-};
-
-export const setIsAdminAction = (isAdmin: boolean): UserAction => {
-  return { type: UserActionType.setIsAdmin, payload: isAdmin };
 };
 
 export const setLoginAction = (user: User, token: string): UserAction => {
@@ -53,7 +26,6 @@ export const setLoginAction = (user: User, token: string): UserAction => {
 export const userReducer = (
   currentState: UserState = {
     user: null,
-    allUsers: [],
     token: null,
   },
   action: UserAction
@@ -61,31 +33,9 @@ export const userReducer = (
   const newState = { ...currentState };
 
   switch (action.type) {
-    case UserActionType.updateUser:
-      newState.user = action.payload;
-      break;
-
-    case UserActionType.getAllUsers:
-      newState.allUsers = action.payload.filter((user: any) => !user.isAdmin);
-      break;
-
-    case UserActionType.deleteUser:
-      newState.allUsers = newState.allUsers.filter(
-        (user) => user._id !== action.payload
-      );
-      break;
-
-    case UserActionType.addUser:
-      newState.allUsers = [...newState.allUsers, action.payload];
-      break;
-
     case UserActionType.setLogout:
       newState.user = null;
       newState.token = null;
-      break;
-
-    case UserActionType.setIsAdmin:
-      newState.isAdmin = action.payload;
       break;
 
     case UserActionType.setLogin:
