46db5c547aca97f77c47c7a67b516b20f48cc263
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-11 10:26:44 +0300

    remove unused route

--- server/src/routes/Employee.ts
+++ server/src/routes/Employee.ts
@@ -5,7 +5,6 @@ import {
   createEmployee,
   updateEmployee,
   deleteEmployee,
-  getEmployeeShifts,
 } from "../controllers/employee";
 
 const router = express.Router();
@@ -14,6 +13,5 @@ router.get("/getAllEmployees", verifyToken, getAllEmployees);
 router.post("/createEmployee", verifyToken, createEmployee);
 router.put("/updateEmployee/:id", verifyToken, updateEmployee);
 router.delete("/deleteEmployee/:id", verifyToken, deleteEmployee);
-router.get("/getEmployeeShifts/:id/shifts", verifyToken, getEmployeeShifts);
 
 export default router;
