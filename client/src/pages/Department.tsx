0e9acc1036cda4f4143dfd480ffd970011283305
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-12 16:51:14 +0300

    re structure  department page

--- client/src/pages/Department.tsx
+++ client/src/pages/Department.tsx
@@ -1,37 +1,20 @@
 import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { TableVirtuoso, TableComponents } from "react-virtuoso";
 import AddDepartment from "../components/AddDepartment";
 import { store } from "../redux/Store";
 import { fetchDeleteDepartment } from "../utils/fetchData";
 import { Department } from "../models/Department";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
 import EditDepartment from "../components/EditDepartment";
 import { deleteDepartmentAction } from "../redux/DepartmentReducer";
 import { useManagerNames } from "../hooks/useManagerNames";
 import { decreaseActionNumberAction } from "../redux/UserReducer";
+import { ColumnData } from "../models/ColumnData";
+import { TableBase } from "../components/TableBase";
 
-interface ColumnData {
-  dataKey: keyof Department | string;
-  label: string;
-  numeric?: boolean;
-  width: number;
-}
-
-const columns: ColumnData[] = [
-  {
-    width: 10,
-    label: "No.",
-    dataKey: "id" as keyof Department,
-  },
+const columns: ColumnData<Department>[] = [
   {
     width: 60,
     label: "Department Name",
@@ -42,38 +25,22 @@ const columns: ColumnData[] = [
     label: "Manger Name",
     dataKey: "manager",
   },
-  {
-    width: 10,
-    label: "Edit",
-    dataKey: "edit" as keyof Department,
-  },
-  {
-    width: 10,
-    label: "Delete",
-    dataKey: "delete" as keyof Department,
-  },
 ];
-
-const VirtuosoTableComponents: TableComponents<Department> = {
-  Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
-    <TableContainer component={Paper} {...props} ref={ref} />
-  )),
-  Table: (props) => (
-    <Table
-      {...props}
-      sx={{ borderCollapse: "separate", tableLayout: "fixed" }}
-    />
-  ),
-  TableHead,
-  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
-  TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
-    <TableBody {...props} ref={ref} />
-  )),
-};
 
 function fixedHeaderContent() {
   return (
     <TableRow>
+      <TableCell
+        sx={{
+          width: 10,
+          backgroundColor: "background.paper",
+          color: "InfoText",
+          fontWeight: "bold",
+        }}
+        variant="head"
+      >
+        No.
+      </TableCell>
       {columns.map((column) => (
         <TableCell
           key={column.dataKey}
@@ -89,68 +56,60 @@ function fixedHeaderContent() {
           <div>{column.label}</div>
         </TableCell>
       ))}
+      <TableCell
+        sx={{
+          width: 10,
+          backgroundColor: "background.paper",
+          color: "InfoText",
+          fontWeight: "bold",
+        }}
+        variant="head"
+      >
+        Edit
+      </TableCell>
+      <TableCell
+        sx={{
+          width: 10,
+          backgroundColor: "background.paper",
+          color: "InfoText",
+          fontWeight: "bold",
+        }}
+        variant="head"
+      >
+        Delete
+      </TableCell>
     </TableRow>
   );
 }
 
-function rowContent(_index: number, row: Department) {
+function rowContent(index: number, row: Department) {
   const handleDelete = (id: string) => {
-    fetchDeleteDepartment(id)
-      .then((response) => {
-        if (!response) {
-          return;
-        }
-        store.dispatch(deleteDepartmentAction(id));
-        store.dispatch(decreaseActionNumberAction());
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    fetchDeleteDepartment(id);
+    store.dispatch(deleteDepartmentAction(id));
+    store.dispatch(decreaseActionNumberAction());
   };
 
   return (
     <React.Fragment>
+      <TableCell>{index + 1}</TableCell>
       {columns.map((column) => {
-        if (column.dataKey === "delete") {
-          return (
-            <TableCell
-              key={column.dataKey}
-              align={column.numeric || false ? "right" : "left"}
-            >
-              <IconButton onClick={() => handleDelete(row._id)}>
-                <DeleteForeverIcon color="error" />
-              </IconButton>
-            </TableCell>
-          );
-        } else if (column.dataKey === "edit") {
-          return (
-            <TableCell
-              key={column.dataKey}
-              align={column.numeric || false ? "right" : "left"}
-            >
-              <EditDepartment departmentId={row._id} />
-            </TableCell>
-          );
-        } else if (column.dataKey === "id") {
-          return (
-            <TableCell
-              key={column.dataKey}
-              align={column.numeric || false ? "right" : "left"}
-            >
-              {_index + 1}
-            </TableCell>
-          );
-        } else {
-          return (
-            <TableCell
-              key={column.dataKey}
-              align={column.numeric || false ? "right" : "left"}
-            >
-              <div>{row[column.dataKey as keyof Department]}</div>
-            </TableCell>
-          );
-        }
+        return (
+          <TableCell
+            key={column.dataKey}
+            align={column.numeric || false ? "right" : "left"}
+          >
+            <div>{row[column.dataKey as keyof Department]}</div>
+          </TableCell>
+        );
       })}
+      <TableCell>
+        <EditDepartment departmentId={row._id} />
+      </TableCell>
+      <TableCell>
+        <IconButton onClick={() => handleDelete(row._id)}>
+          <DeleteForeverIcon color="error" />
+        </IconButton>
+      </TableCell>
     </React.Fragment>
   );
 }
@@ -158,32 +117,12 @@ function rowContent(_index: number, row: Department) {
 export default function ReactVirtualizedTable() {
   const departmentsWithManagerNames = useManagerNames();
   return (
-    <Box
-      className="background"
-      style={{ height: "100%", display: "flex", flexDirection: "column" }}
-    >
-      <Box sx={{ p: 3 }}>
-        <AddDepartment />
-      </Box>
-      <Box
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-          marginBottom: 400,
-        }}
-      >
-        <Paper style={{ height: 450, width: "70%" }}>
-          <TableVirtuoso
-            data={departmentsWithManagerNames}
-            components={VirtuosoTableComponents}
-            fixedHeaderContent={fixedHeaderContent}
-            itemContent={rowContent}
-          />
-        </Paper>
-      </Box>
-    </Box>
+    <TableBase
+      columns={columns}
+      fixedHeaderContent={fixedHeaderContent}
+      rowContent={rowContent}
+      data={departmentsWithManagerNames}
+      ExtraComponent={AddDepartment}
+    />
   );
 }
