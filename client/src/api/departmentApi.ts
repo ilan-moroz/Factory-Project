2cceb88c9854eaf02e5846b74fca36e5a77c983f
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-14 15:31:05 +0300

    comments

--- client/src/api/departmentApi.ts
+++ client/src/api/departmentApi.ts
@@ -1,10 +1,12 @@
 import api from "./apiConfig";
 
+// Function to add a department
 export const addDepartment = async (
   departmentName: string,
   manager: string
 ) => {
   try {
+    // Make a POST request to add a new department
     const response = await api.post("/departments/addDepartment", {
       name: departmentName,
       manager: manager,
@@ -15,8 +17,10 @@ export const addDepartment = async (
   }
 };
 
+// Function to get all departments
 export const getAllDepartments = async () => {
   try {
+    // Make a GET request to retrieve all departments
     const response = await api.get("/departments/getAllDepartments");
     if (response.status === 200) return response.data;
   } catch (err) {
@@ -24,8 +28,10 @@ export const getAllDepartments = async () => {
   }
 };
 
+// Function to delete a department
 export const deleteDepartment = async (id: string) => {
   try {
+    // Make a DELETE request to remove a department
     const response = await api.delete(`/departments/deleteDepartment/${id}`);
     if (response.status === 200) return response.data;
   } catch (err) {
@@ -33,12 +39,14 @@ export const deleteDepartment = async (id: string) => {
   }
 };
 
+// Function to update a department
 export const updateDepartment = async (
   departmentId: string,
   departmentName: string,
   manager: string
 ) => {
   try {
+    // Make a PUT request to update a department
     const response = await api.put(
       `/departments/updateDepartment/${departmentId}`,
       {
