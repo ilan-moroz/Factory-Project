b02443e133026a19ea5837c2cd33bef5f55df1ab
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-14 15:28:03 +0300

    comments

--- client/src/api/apiConfig.ts
+++ client/src/api/apiConfig.ts
@@ -1,22 +1,27 @@
 import axios from "axios";
 
 const baseUrl = "http://localhost:4000";
+
+// Create an instance of axios with the base URL
 const api = axios.create({
   baseURL: baseUrl,
 });
 
+// Use axios interceptors to alter requests before they are sent
 api.interceptors.request.use(
   (config) => {
+    // Retrieve the persisted users from local storage
     const persistedUsers = localStorage.getItem("persist:users");
     let token;
+
     if (persistedUsers) {
       const users = JSON.parse(persistedUsers);
       if (users.token) {
-        // Remember to parse the token if it's still a JSON string
         token = JSON.parse(users.token);
       }
     }
     if (token) {
+      // Add the token to the request headers
       config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
