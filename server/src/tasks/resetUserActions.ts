aeacab5754ae65602546f8eca146ed248000707a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-15 12:54:49 +0300

    // Update all users where isAdmin is false

--- server/src/tasks/resetUserActions.ts
+++ server/src/tasks/resetUserActions.ts
@@ -5,8 +5,11 @@ import { User } from "../models/Factory";
 export const resetUserActions = () => {
   cron.schedule("0 0 * * *", async function () {
     try {
-      // Update all users
-      await User.updateMany({}, { $set: { numOfActions: 10 } });
+      // Update all users where isAdmin is false
+      await User.updateMany(
+        { isAdmin: { $ne: true } },
+        { $set: { numOfActions: 10 } }
+      );
       console.log("User actions reset");
     } catch (err) {
       console.error("Error resetting user actions: ", err);
