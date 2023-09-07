b11d358526d0fce30767c5dc151431e14a767356
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-07 15:35:25 +0300

    createNewUser route

--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, userNumOfActions } from "../controllers/User";
+import { createNewUser, login, userNumOfActions } from "../controllers/User";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/register", createNewUser);
 router.get("/numOfActions", userNumOfActions);
 
 export default router;
